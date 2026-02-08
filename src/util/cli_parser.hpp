ff572b4c7a729679c7d46c36cc1117460e4c96f8
Daniel Brommer <daniel.brommer@student.kit.edu>
2026-02-08 15:01:54 +0100

    feat: fix cli arguments

--- src/util/cli_parser.hpp
+++ src/util/cli_parser.hpp
@@ -16,10 +16,9 @@ Params read_parameters(int argc, char const* argv[]) {
     CLI::App app{"Prefix Free Parsing"};
 
     Params params;
-    app.add_option("--input", params.input_path, "Path to input file");
-    app.add_option("--w", params.window_size, "Window size");
-    app.add_option("--hash", params.hash, "Hash function to use");
-    app.add_option("--p", params.p_mod, "Modulo for hash splitters");
+    app.add_option("--input", params.input_path, "Path to input file")->required();
+    app.add_option("-w", params.window_size, "Window size");
+    app.add_option("-p", params.p_mod, "Modulo for hash splitters");
     app.add_option("--verbose", params.verbose, "Verbose output");
 
     CLI11_PARSE_MPI(app, argc, argv);
