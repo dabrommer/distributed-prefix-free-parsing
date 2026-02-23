8ed1785b33a7df078b4fbfd8a5223056370a9db3
Daniel Brommer <daniel.brommer@student.kit.edu>
2026-02-23 10:25:21 +0100

    refactor: make check_sa generic

--- src/checks/check_sa.hpp
+++ src/checks/check_sa.hpp
@@ -9,7 +9,8 @@
 
 using namespace kamping;
 
-bool check_sa(std::vector<dsss::UIntPair<uint8_t>>& sa, std::vector<uint32_t>& data, Communicator<>& comm) {
+template<typename SAType, typename T>
+bool check_sa(std::vector<SAType>& sa, T& data, Communicator<>& comm) {
 
     // Gather SA and data on root
     auto sa_complete = comm.gatherv(send_buf(sa));
@@ -19,11 +20,26 @@ bool check_sa(std::vector<dsss::UIntPair<uint8_t>>& sa, std::vector<uint32_t>& d
     }
     // Check SA on root
     int index = 0;
-    uint64_t prev_sa_index = sa_complete.front().u64();
-    for (auto const& pair: sa_complete) {
-        auto curr_sa_index = pair.u64();
+    uint64_t prev_sa_index;
+    if constexpr (std::is_same_v<SAType, dsss::UIntPair<uint8_t>>) {
+        prev_sa_index = sa_complete.front().u64();
+    }
+    else {
+        prev_sa_index = sa_complete.front();
+    }
+    for (auto& sa_value: sa_complete) {
+        std::string prev_string;
+        std::string curr_string;
+        uint64_t curr_sa_index;
+        if constexpr (std::is_same_v<SAType, dsss::UIntPair<uint8_t>>) {
+            curr_sa_index = sa_value.u64();
+        }
+        else {
+            curr_sa_index = sa_value;
+        }
         for (auto [prev, curr] = std::tuple{prev_sa_index, curr_sa_index}; prev < data_complete.size() && curr < data_complete.size(); ++prev, ++curr)  {
-
+            prev_string.push_back(static_cast<char>(data_complete[prev]));
+            curr_string.push_back(static_cast<char>(data_complete[curr]));
             if (data_complete[prev] == data_complete[curr]) {
                 continue;
             } else if (data_complete[prev] < data_complete[curr]) {
